262213b68d06046f188e2936f83c960b2f7bb7ac
shahrzad011 <shahrzad.zandi011@gmail.com>
2026-07-31 15:46:12 +0330

    feat(models): update product model relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,8 +10,10 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\ProductStatus;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Builder;
+
 /**
  * Class Product
  *
@@ -34,77 +36,85 @@ use Illuminate\Database\Eloquent\Builder;
  *
  * @package App\Models
  */
-
-
 class Product extends Model
 {
-	use SoftDeletes;
-	protected $table = 'products';
-	public static $snakeAttributes = false;
+    use SoftDeletes;
 
-	protected $casts = [
-		'product_category_id' => 'int',
-		'price' => 'int',
-		'discount' => 'float',
-		'qty' => 'int',
-		'status' => 'int'
-	];
+    protected $table = 'products';
+    public static $snakeAttributes = false;
 
-	protected $fillable = [
-		'name',
-		'en_name',
-		'product_category_id',
-		'price',
-		'discount',
-		'qty',
-		'status',
-		'description'
-	];
+    protected $casts = [
+        'product_category_id' => 'int',
+        'price' => 'int',
+        'discount' => 'float',
+        'qty' => 'int',
+        'status' => ProductStatus::class
+    ];
 
-	public function productCategory()
-	{
-		return $this->belongsTo(ProductCategory::class);
-	}
+    protected $fillable = [
+        'name',
+        'en_name',
+        'product_category_id',
+        'price',
+        'discount',
+        'qty',
+        'status',
+        'description'
+    ];
 
-	public function orderItems()
-	{
-		return $this->hasMany(OrderItem::class);
-	}
+    public function productCategory()
+    {
+        return $this->belongsTo(ProductCategory::class, 'product_category_id');
+    }
 
-	public function productImages()
-	{
-		return $this->hasMany(ProductImage::class);
-	}
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function productImages()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function defaultImage()
+    {
+        return $this->hasOne(ProductImage::class)
+            ->where('is_default', true);
+    }
 
 
     #[Scope]
     public function applySort(Builder $query): void
     {
-        $request = request();
+
+        $sort = request()->input('sort');
 
 
-        switch ($request->input('sort') ?? 'unk') {
-            case 'best_selling':
-            {
-                $query->withSum('orderItems', 'qty')
-                    ->orderByDesc('order_items_sum_qty');
+        switch ($sort) {
+
+            case 'name_asc':
+                $query->orderBy('name');
                 break;
 
-            }
-            case 'lowest':
-            {
+
+            case 'name_desc':
+                $query->orderByDesc('name');
+                break;
+
+
+            case 'price_asc':
                 $query->orderBy('price');
                 break;
-            }
-            case 'highest':
-            {
+
+
+            case 'price_desc':
                 $query->orderByDesc('price');
                 break;
-            }
+
+
             default:
-            {
                 $query->orderByDesc('created_at');
-            }
         }
     }
 
@@ -133,10 +143,12 @@ class Product extends Model
     #[Scope]
     public function applySearch(Builder $query): void
     {
+
         $request = request();
 
-        if ($request->filled('keyword')) {
-            $keyword = $request->input('keyword');
+        if ($request->filled('search')) {
+
+            $keyword = $request->input('search');
 
             $query->whereAny([
                 'name',
@@ -145,7 +157,5 @@ class Product extends Model
             ], 'LIKE', "%$keyword%");
         }
 
-
     }
-
 }
