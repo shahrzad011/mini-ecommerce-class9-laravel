822226b2fe018494d664469b61317ac0816cfa21
shahrzad011 <shahrzad.zandi011@gmail.com>
2026-07-31 15:51:07 +0330

    feat(checkout): update checkout process

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Exceptions\CartItemNotFoundException;
+use App\Services\CartService;
+use App\Services\OrderService;
+use App\Http\Requests\CheckoutPostRequest;
+use Illuminate\Support\Facades\Log;
 
 class CheckoutController extends Controller
 {
-    //
+    public function index()
+    {
+        $userCartItems = CartService::getItemsWithDetails();
+
+        $userCartTotalPrices = CartService::getTotalPrices();
+
+        return view('checkout', compact('userCartItems', 'userCartTotalPrices'));
+    }
+    public function post(CheckoutPostRequest $request)
+    {
+        $checkoutData = $request->only([
+            'user_province',
+            'user_city',
+            'user_address',
+            'user_postal_code',
+            'user_mobile',
+            'description'
+
+        ]);
+
+        try {
+            OrderService::register($checkoutData);
+        }catch (CartItemNotFoundException $exception){
+            return back()->withErrors([
+                'general'=>$exception->getMessage()
+            ]);
+        }catch (\Exception $exception){
+            Log::error($exception);
+
+            return back()->withErrors([
+                'general'=>'خطایی رخ داده است، با پشتیبانی ارتباط بگیرید'
+            ]);
+        }
+        return redirect()->route('account.orders');
+    }
 }
