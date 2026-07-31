0e7c3dc9bdcccaf3c5faeb21f41e33e2e812d56e
shahrzad011 <shahrzad.zandi011@gmail.com>
2026-07-31 16:00:00 +0330

    chore(bootstrap): update application bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,23 +3,37 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web:[
-            __DIR__.'/../routes/web.php',
-            __DIR__.'/../routes/auth.php',
+        web: [
+            __DIR__ . '/../routes/web.php',
+            __DIR__ . '/../routes/auth.php',
+            __DIR__ . '/../routes/admin.php',
 
 
         ],
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
 
-            $middleware->redirectGuestsTo(fn (Illuminate\Http\Request $request) => route('auth.login.index'));
+        $middleware
+            ->redirectGuestsTo(function (Request $request) {
+                if ($request->routeIs('admin.*')) {
+                    return route('admin.auth.login.index');
+                }
+                return route('auth.login.index');
+            })
 
-            $middleware->redirectUsersTo(fn (Illuminate\Http\Request $request) => route('index'));
+            ->redirectUsersTo(function (Request $request) {
+                if ($request->routeIs('admin.*')) {
+                    return route('admin.dashboard.index');
+                }
+                return route('index');
+            });
+
 
 
     })
