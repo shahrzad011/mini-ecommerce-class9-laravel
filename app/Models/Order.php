427f1f6f8c4958da99f036693c8b9bbcc8198661
shahrzad011 <shahrzad.zandi011@gmail.com>
2026-07-31 15:45:17 +0330

    feat(models): update order model logic

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,13 +7,15 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Order
- * 
+ *
  * @property int $id
  * @property int $user_id
  * @property int $final_price
@@ -30,7 +32,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property Carbon $created_at
  * @property Carbon $updated_at
  * @property string|null $deleted_at
- * 
+ *
  * @property User $user
  * @property Collection|OrderItem[] $orderItems
  *
@@ -38,40 +40,92 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Order extends Model
 {
-	use SoftDeletes;
-	protected $table = 'orders';
-	public static $snakeAttributes = false;
+    use SoftDeletes;
 
-	protected $casts = [
-		'user_id' => 'int',
-		'final_price' => 'int',
-		'final_discount' => 'int',
-		'total_products' => 'int',
-		'status' => 'int'
-	];
+    protected $table = 'orders';
+    public static $snakeAttributes = false;
 
-	protected $fillable = [
-		'user_id',
-		'final_price',
-		'final_discount',
-		'total_products',
-		'user_province',
-		'user_city',
-		'user_address',
-		'user_postal_code',
-		'user_mobile',
-		'description',
-		'tracking_code',
-		'status'
-	];
+    protected $casts = [
+        'user_id' => 'int',
+        'final_price' => 'int',
+        'final_discount' => 'int',
+        'total_products' => 'int',
+        'status' => 'int'
+    ];
 
-	public function user()
-	{
-		return $this->belongsTo(User::class);
-	}
+    protected $fillable = [
+        'user_id',
+        'final_price',
+        'final_discount',
+        'total_products',
+        'user_province',
+        'user_city',
+        'user_address',
+        'user_postal_code',
+        'user_mobile',
+        'description',
+        'tracking_code',
+        'status'
+    ];
 
-	public function orderItems()
-	{
-		return $this->hasMany(OrderItem::class);
-	}
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    #[Scope]
+    public function scopeSearch(Builder $query, $search)
+    {
+
+        if (!$search) {
+            return;
+        }
+
+        return $query->where(function ($q) use ($search) {
+
+            $q->where('id', $search)
+                ->orWhere('user_mobile', 'like', "%$search%");
+
+        });
+
+    }
+
+    #[Scope]
+    public function scopeSort(Builder $query, $sort)
+    {
+
+        switch ($sort) {
+
+            case 'price_high':
+
+                return $query->orderByDesc('final_price');
+
+
+            case 'price_low':
+
+                return $query->orderBy('final_price');
+
+
+            case 'status':
+
+                return $query->orderBy('status');
+
+
+            case 'created_at_asc':
+
+                return $query->oldest();
+
+
+            default:
+
+                return $query;
+
+        }
+
+    }
 }
