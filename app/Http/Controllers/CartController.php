b6853308c18131458f21497d3e4723a59b7b5666
shahrzad011 <shahrzad.zandi011@gmail.com>
2026-07-31 15:50:08 +0330

    feat(cart): update cart controller functionality

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderAddRequest;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function index()
+    {
+        $userCartItems = CartService::getItemsWithDetails();
+
+        $userCartTotalPrices = CartService::getTotalPrices();
+
+        return view('cart', compact('userCartItems', 'userCartTotalPrices'));
+
+    }
+    public function add(OrderAddRequest $request)
+    {
+
+        CartService::add(
+            $request->input('product_id'),
+            $request->input('qty'),
+
+        );
+        return redirect()->back();
+    }
+    public function removeItem(int $productId)
+    {
+
+        CartService::remove($productId);
+        return back();
+    }
+    public function clear()
+    {
+        CartService::clean();
+
+        return back();
+    }
+    public function updateQty(Request $request)
+    {
+        $cartItems = $request->input('cart_items');
+        foreach ($cartItems as $cartItem){
+            CartService::updateQty($cartItem['product_id'], $cartItem['qty']);
+        }
+        return back();
+    }
 }
